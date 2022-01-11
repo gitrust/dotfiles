4d69cdd896cb6acf4b3bf90c6347daa69d32bca2
gitrust <weebac@web.de>
2022-01-11 09:05:20 +0100

    Use HOME variable in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 #git pull origin master;
 
 function doIt() {
+  echo "Sync dotfiles to $HOME"
   rsync --exclude ".git/" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE.txt" \
-		-avh --no-perms . ~;
-  source ~/.bash_profile;
+		-avh --no-perms . $HOME;
+  source $HOME/.bash_profile;
 }
 
 # parameter -force/-f will override without prompt
